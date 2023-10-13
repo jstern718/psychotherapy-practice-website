87f6ad39a781021a08a0eeefea3b0e9764312bba
Jonathan Stern <jstern718@gmail.com>
2023-10-12 22:53:36 -0400

    form on contact page

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,12 @@ import React from "react"
 
 function Contact() {
     return (
-        <div></div>
+        <div className="m-5 p-5">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSem7vFa2LsUSnoLZuGJf4eq5yGyW2uuwUwPGpSnKH41UkUJqA/viewform?embedded=true"
+            width="640" height="1012" frameborder="0" marginheight="0" marginwidth="0">
+                Loading…
+            </iframe>
+        </div>
         );
   }
 
